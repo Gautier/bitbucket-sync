9275fa9860053b7d15f12d365c5ad605a720ad53
Gautier Hayoun <ghayoun@gmail.com>
2013-10-22 17:03:37 +0100

    Rewrite process handling

--- bitbucket_sync/main.py
+++ bitbucket_sync/main.py
@@ -13,62 +13,106 @@ Options:
   --processes=<processes>    Number of repositories to mirror at the same time (defaults to CPU count)
 """
 
-from multiprocessing import Pool
+from Queue import Queue
+from multiprocessing import cpu_count
+from threading import Thread
 import os
-import signal
+import shutil
 import subprocess
 import sys
+import thread
+import time
 
 from docopt import docopt
 from requests_oauthlib import OAuth1
 import requests
 
-def sync_repo(args):
-    directory, repo = args
-    os.chdir(directory)
+def sync_repo(directory, repo, lock):
+    git_dir = os.path.join(directory, "%s.git" % repo["slug"])
     repo_url = "git@bitbucket.org:%s/%s.git" % (repo["owner"], repo["slug"])
-    try:
-        subprocess.call(["git", "clone", "--mirror", repo_url])
-        os.chdir("%s.git" % repo["slug"])
-        subprocess.call(["git", "fetch", "-q"])
-    except Exception:
-        raise
 
-def init_worker():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    try:
+        subprocess.check_output(["git", "--git-dir", git_dir, "rev-parse"],
+                                stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        # git repository hasn't yet been cloned properly
+        if os.path.exists(git_dir):
+            shutil.rmtree(git_dir)
+
+        try:
+            subprocess.check_output(["git",
+                                     "clone",
+                                     "--mirror",
+                                     repo_url,
+                                     git_dir],
+                                     stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError, e:
+            lock.acquire()
+            print("")
+            print("Couldn't clone %s" % repo_url)
+            print("git command: %s" % " ".join(e.cmd))
+            print("git output:\n%s" % e.output)
+            print("")
+            lock.release()
+    else:
+        # git repository is valid
+        subprocess.call(["git", "--git-dir", git_dir, "fetch", "-q"])
+
+
+def worker(repositories, lock):
+    while not repositories.empty():
+        directory, repo = repositories.get()
+        sync_repo(directory, repo, lock)
+
 
 def main():
-    arguments = docopt(__doc__, argv=sys.argv[1:], help=True, version="0.1")
+    arguments = docopt(__doc__, argv=sys.argv[1:], help=True, version="0.3.0")
 
     key = arguments["--key"]
     secret = arguments["--secret"]
     directory = arguments["--directory"]
-    processes = arguments["--processes"]
+    try:
+        processes = int(arguments["--processes"] or "")
+    except ValueError:
+        processes = cpu_count()
 
     oauth = OAuth1(client_key=unicode(key), client_secret=unicode(secret))
     API_ROOT = 'https://api.bitbucket.org'
     deploy_keys_resource = "%s/1.0/user/repositories/" % API_ROOT
     response = requests.get(deploy_keys_resource, auth=oauth)
     if response.status_code != 200:
-        print "Error while listing the repositories"
+        print("Error while listing the repositories")
         sys.exit(1)
 
     repo_list = response.json()
     only_git = [repo for repo in repo_list if repo["scm"] == "git"]
 
+    directory = os.path.abspath(directory)
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     if not os.path.isdir(directory):
-        print "%s is not a directory" % directory
+        print("%s is not a directory" % directory)
         sys.exit(1)
 
-    pool = Pool(processes=processes, initializer=init_worker)
-    directory = os.path.abspath(directory)
+    repositories = Queue()
+    for repo in only_git:
+        repositories.put((directory, repo))
+
+    lock = thread.allocate_lock()
+    worker_threads = []
+    for i in range(processes):
+        worker_thread = Thread(target=worker, args=(repositories, lock))
+        worker_thread.start()
+        worker_threads.append(worker_thread)
 
     try:
-        pool.map(sync_repo, [(directory, repo) for repo in only_git])
+        while any(w.isAlive() for w in worker_threads):
+            time.sleep(.5)
     except KeyboardInterrupt:
-        pool.close()
-        pool.terminate()
-        pool.join()
+        # purge rest of queue
+        # Note that the git processes inside the workder threads should get
+        # killed
+        while not repositories.empty():
+            repositories.get()
