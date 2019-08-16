570ceadb34b20b52072c9f891d763c3287995b78
cli-guy <48386230+cli-guy@users.noreply.github.com>
2019-08-16 15:31:41 +0100

    Update main.py

--- bitbucket_sync/main.py
+++ bitbucket_sync/main.py
@@ -70,44 +70,48 @@ class HgCommands(object):
         subprocess.call(["hg", "-R", self.local_dir, "pull", "-q"])
 
 
-def sync_repo(directory, repo, owner, lock):
-    if repo["scm"] == "git":
-        scm = GitCommands(directory, owner, repo["slug"])
-    elif repo["scm"] == "hg":
-        scm = HgCommands(directory, owner, repo["slug"])
+def sync_repo(directory, scm, slug, owner, lock):
+    if scm == "git":
+        scmTool = GitCommands(directory, owner, slug)
+    elif scm == "hg":
+        scmTool = HgCommands(directory, owner, slug)
     else:
         raise NotImplementedError(
-                "SCM of type %s is not currently supported" % repo["scm"])
+                "SCM of type %s is not currently supported" % scm)
 
     try:
-        scm.validate_local_repository()
+        scmTool.validate_local_repository()
     except subprocess.CalledProcessError:
         # git repository hasn't yet been cloned properly
-        if os.path.exists(scm.local_dir):
-            shutil.rmtree(scm.local_dir)
+        if os.path.exists(scmTool.local_dir):
+            shutil.rmtree(scmTool.local_dir)
 
         try:
-            scm.clone()
+            scmTool.clone()
             return True
         except subprocess.CalledProcessError, e:
             lock.acquire()
             print("")
-            print("Couldn't clone %s" % scm.repo_url)
+            print("Couldn't clone %s" % scmTool.repo_url)
             print("command: %s" % " ".join(e.cmd))
             print("output:\n%s" % e.output)
             print("")
             lock.release()
     else:
         # git repository is valid
-        scm.update()
+        scmTool.update()
         return True
 
 
 def worker(repositories, lock, owner):
     while not repositories.empty():
         directory, repo = repositories.get()
-        if sync_repo(directory, repo, owner, lock):
-            print "%s/%s synchronised" % (owner, repo["slug"])
+        if owner == "":
+            slug = repo['full_name']
+        else:
+            slug = repo['slug']
+        if sync_repo(directory, repo["scm"], slug, owner, lock):
+            print "%s/%s synchronised" % (owner, slug)
 
 
 def ensure_base_directory(directory):
@@ -122,30 +126,35 @@ def ensure_base_directory(directory):
 
     return directory
 
-
-def retrieve_repositories_list_pages(key, secret, owner, nextP=""):
-    oauth = OAuth1(client_key=unicode(key), client_secret=unicode(secret))
-    API_ROOT = 'https://api.bitbucket.org'
-    deploy_keys_resource = "%s/2.0/repositories/%s/%s" % (API_ROOT, owner, nextP)
-    response = requests.get(deploy_keys_resource, auth=oauth)
+def check_request(url, oauth):
+    response = requests.get(url, auth=oauth)
     if response.status_code != 200:
         print("Error while listing the repositories")
+        print(response.status_code, response.text)
         sys.exit(1)
-
     return response
 
-def retrieve_repositories_list(key, secret, owner, nextP=""):
+def retrieve_repositories_list_pages(key, secret, owner, next_page=""):
+    oauth = OAuth1(client_key=unicode(key), client_secret=unicode(secret))
+    API_ROOT = 'https://api.bitbucket.org'
+    if owner == "":
+        user = check_request("%s/2.0/user" % API_ROOT, oauth).json()['username']
+        deploy_keys_resource = "%s/2.0/repositories/%s?role=member&%s" % (API_ROOT, user, next_page)
+    else:
+        deploy_keys_resource = "%s/2.0/repositories/%s/%s" % (API_ROOT, owner, next_page)
+    return check_request(deploy_keys_resource, oauth)
+
+def retrieve_repositories_list(key, secret, owner, next_page=""):
     resp = []
-    page =""
+    page = ""
     while True:
-        r = retrieve_repositories_list_pages(key, secret, owner, nextP=page).json()
-        resp += r['values']
-        if 'next' in r:
-            page = r['next'].split('/')[-1]
+        response = retrieve_repositories_list_pages(key, secret, owner, next_page=page).json()
+        for repository in response['values']:
+            yield repository
+        if 'next' in response:
+            page = response['next'].split('/')[-1]
         else:
             break
-    for repository in resp:
-        yield repository
  
 
 def configure_queue(api_repositories, directory):
